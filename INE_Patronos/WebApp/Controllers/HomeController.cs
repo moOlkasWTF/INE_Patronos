9a981984fed4624bdd8c25490ab4b1d863781706
GerardoJGuerra <301710033@bis.ute.edu.mx>
2018-11-07 10:16:51 -0600

    3 tablas de models, dbContext y dbInitializer

--- INE_Patronos/WebApp/Controllers/HomeController.cs
+++ INE_Patronos/WebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -10,6 +11,11 @@ namespace WebApp.Controllers
     {
         public ActionResult Index()
         {
+            using (INE_PatronosDbContext dbCtx = new INE_PatronosDbContext())
+            {
+                new INE_PatronosDbInitializer().InitializeDatabase(dbCtx);
+            }
+
             return View();
         }
 
---
+++ INE_Patronos/WebApp/Models/INE_PatronosDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class INE_PatronosDbContext : DbContext
+    {
+
+        public INE_PatronosDbContext() : base("INE_PatronosDbContext")
+        {
+            Database.SetInitializer<INE_PatronosDbContext>(new INE_PatronosDbInitializer());
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<CountryRegion> CountriesRegion { get; set; }
+        public DbSet<StateProvince> StatesProvince { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
